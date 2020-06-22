5c6a1366f5f57f8edee3399dd70ff723a819e5f6
Joshua Christensen <joshua@thedigitaldesign.com>
2020-06-21 21:29:08 -0600

    trying to get things working betta

---
+++ src/templates/Post.vue
@@ -0,0 +1,33 @@
+<template>
+  <BlogLayout>
+    <div>
+      <h1>{{$page.post.title}}</h1>
+      <img :src="$page.post.featuredImage" alt="blog">
+      <div v-html="$page.post.content"></div>
+    </div>
+  </BlogLayout>
+</template>
+
+<page-query>
+  query Post($path: String!) {
+    post: post(path: $path) {
+      title,
+      content,
+      featuredImage
+    }
+  }
+</page-query>
+
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.post.title
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
